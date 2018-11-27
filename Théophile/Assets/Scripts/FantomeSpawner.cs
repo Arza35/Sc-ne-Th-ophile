902a1a58222f45fa89bf3bedc29a30a2b4b25ad6
PC Portable Hugo <hugo.raguin@gmail.com>
2018-11-27 22:10:44 +0100

    DebutFantome

---
+++ Théophile/Assets/Scripts/FantomeSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FantomeSpawner : MonoBehaviour {
+
+    public float spawnRate;
+    public GameObject Fantome;
+    public bool gizmos = true;
+
+    public float xOffset;
+
+    public float yOffset;
+
+	void Start () {
+        StartCoroutine(beginSpawn());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator beginSpawn() {
+        yield return new WaitForSeconds(spawnRate);
+        Instantiate(Fantome, new Vector2(Mathf.Clamp(transform.position.x, transform.position.x - xOffset, transform.position.x + xOffset), Mathf.Clamp(transform.position.y, transform.position.y - yOffset, transform.position.y + yOffset)), Quaternion.identity);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (gizmos) {
+
+        }
+    }
+}

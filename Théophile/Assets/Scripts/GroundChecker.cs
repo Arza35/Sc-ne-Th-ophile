40c577d47b4aca5d2615ace1e14bdc330bd9429b
PC Portable Hugo <hugo.raguin@gmail.com>
2018-11-27 14:39:11 +0100

    Ajout Scripts

---
+++ Théophile/Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour {
+
+    private PlayerControllerPlat playerController;
+
+	void Start () {
+        playerController = FindObjectOfType<PlayerControllerPlat>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Sol"))
+        {
+            playerController.Grounded();
+        }
+    }
+}
---
+++ Théophile/Assets/Scripts/PlayerControllerPlat.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerPlat : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    public float speed;
+
+    public float jumpForce;
+    public int howManyJumps;
+    private int jumpCounter;
+    private bool grounded = true;
+    private float h;
+    private bool facingRight = false;
+
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        jumpCounter = howManyJumps;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Mouvement();
+        UpdateAnimator();
+        if ((facingRight && rb2d.velocity.x < 0) || (!facingRight && rb2d.velocity.x > 0f)) {
+            Flip();
+        }
+        
+    }
+
+    void Mouvement() {
+        h = Input.GetAxisRaw("Horizontal");
+
+        rb2d.velocity = new Vector2(h * speed, rb2d.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCounter > 0)
+        {
+            anim.SetTrigger("Jump");
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+            rb2d.AddForce(new Vector2(0, jumpForce));
+            jumpCounter--;
+            grounded = false;
+        }
+    }
+    void UpdateAnimator() {
+        if (rb2d.velocity.y < 0)
+        {
+            anim.SetBool("isFalling", true);
+        }
+        else
+        {
+            anim.SetBool("isFalling", false);
+        }
+        if (h == 0)
+        {
+            anim.SetBool("isMoving", false);
+        }
+        else {
+            anim.SetBool("isMoving", true);
+        }
+        anim.SetBool("Grounded", grounded);
+    }
+    public void Grounded() {
+        jumpCounter = howManyJumps;
+        grounded = true;
+    }
+
+    void Flip() {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+        facingRight = !facingRight;
+    }
+}
